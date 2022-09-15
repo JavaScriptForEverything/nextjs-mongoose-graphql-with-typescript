f4f3f0279ba5c43fc8d3a4c04b272a98423d6813
Riajul Islam <javascriptforeverything@gmail.com>
2022-09-16 01:45:14 +0600

    fixed TypeScript error

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,9 +1,10 @@
 import { ApolloServer } from 'apollo-server-micro'
-import cors from 'micro-cors'
+import Cors from 'micro-cors'
 import database from '@server/models/database'
 
 import typeDefs from '@graphql/typeDefs'
 import resolvers from '@graphql/resolvers'
+const cors = Cors()
 
 database() 		// create database connection
 
@@ -12,12 +13,12 @@ database() 		// create database connection
 const server = new ApolloServer({ typeDefs, resolvers })
 const startServer = server.start()
 
-const handler = async ( req, res) => {
+const handler = cors(async ( req, res) => {
 	if(req.method === 'OPTIONS') return res.end()
 
 	await startServer
 	await server.createHandler({ path: '/api/graphql' })(req, res)
-}
+})
 
-export default cors()(handler)
+export default handler
 export const config = { api: { bodyParser: false, } }
